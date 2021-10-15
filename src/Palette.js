5fd81f06c24641291dfb4fde0e78d4637a5d6504
ih <ihaddy@gmail.com>
2021-10-15 07:36:22 -0400

    add palette.js

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar goes here */}
+                <div className="Palette-colors"></div>
+                <h1>
+                    Palette
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Palette;
