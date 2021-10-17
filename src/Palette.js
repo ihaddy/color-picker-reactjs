4273857fa3d16f6f451f688f6d850cbde648c8a0
ih <ihaddy@gmail.com>
2021-10-17 08:57:33 -0400

    add Palette.css and apply basic styling in palette and colorbox components

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import ColorBox from './ColorBox';
 
 class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background= {color.color} name={color.name}/>
+        ))
         return (
             <div className="Palette">
                 {/* navbar goes here */}
-                <div className="Palette-colors"></div>
-                <h1>
-                    Palette
-                </h1>
+                <div className="Palette-colors">
+                    {colorBoxes}
+                </div>
+                
             </div>
         )
     }
