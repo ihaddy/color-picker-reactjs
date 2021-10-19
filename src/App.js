568e284719b6df93ba2a5c8d86d0fd7dbcfdf9be
ih <ihaddy@gmail.com>
2021-10-19 11:30:21 -0400

    add oncopy click css styling and animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import Palette from "./Palette"
+import ColorBox from './ColorBox';
 import seedColors from './seedColors';
+
+
 function App() {
   return (
     <div>
