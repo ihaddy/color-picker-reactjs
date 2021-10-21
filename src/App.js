7b7fb6d58d93f22b722243d883188f24aa465000
ih <ihaddy@gmail.com>
2021-10-21 07:52:57 -0400

    finish chromahelper.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import Palette from "./Palette"
 import ColorBox from './ColorBox';
 import seedColors from './seedColors';
-
+import generatePalette from './chromaHelpers';
 
 function App() {
+  console.log(generatePalette(seedColors[1]))
   return (
     <div>
         <Palette {...seedColors[4]}/>
-    </div>
+    </div> 
+    
   );
 }
 
