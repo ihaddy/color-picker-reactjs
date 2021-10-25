95b4a3c8e7695aa497531a2adf29ce0c924ab7a3
ih <ihaddy@gmail.com>
2021-10-25 08:45:17 -0400

    add slider enable functionality

--- src/Palette.js
+++ src/Palette.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox';
-import './Palette.css';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 class Palette extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {level: 500}
-    }
-    
-    render() {
-        const {colors} = this.props.palette;
-        const colorBoxes = colors[300].map(color => (
-            <ColorBox background= {color.hex} name={color.name}/>
-            
-        ))
-        console.log(this.props)
-        return (
-            <div className="Palette">
-                <Slider />
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this)
+  }
+  changeLevel(level) {
+      this.setState({level})
+  }
+  render() {
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+    console.log(this.props);
+    return (
+      <div className="Palette">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
+        <div className="Palette-colors">{colorBoxes}</div>
+      </div>
+    );
+  }
 }
 
 //padding comment for now
