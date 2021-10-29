826659c683350cf9e8fc82421177659c77b28549
ih <ihaddy@gmail.com>
2021-10-28 20:58:35 -0400

    create navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <header className="Navbar">
+                <div className="logo">
+                    <a href="/"></a>
+                </div>
+                
+            </header>
+        )
+    }
+}
+
+export default Navbar
