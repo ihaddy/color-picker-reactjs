f9e32016733bce2a8f6257fbe363460560b56bfb
ih <ihaddy@gmail.com>
2021-10-30 07:04:58 -0400

    finish navbar refactor, squash bugs, add navbar.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,30 @@
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Palette.css";
+import "./Navbar.css"
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
     render() {
+        const {level, changeLevel} = this.props
         return (
             <header className="Navbar">
                 <div className="logo">
                     <a href="/">reactcolorpicker</a>
                 </div>
+            <div className="slider-container">
+                <span>Level: {level}</span>
+            <div className="slider">
                 <Slider
                     defaultValue={level}
                     min={100}
                     max={900}
                     step={100}
-                    onAfterChange={this.changeLevel}
-          />
+                    onAfterChange={changeLevel}
+                />
+            </div>
+            </div>
             </header>
         )
     }
 }
-
-export default Navbar
