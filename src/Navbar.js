0dff3c52c2faa98d492f2e34720de166c08f3196
ih <ihaddy@gmail.com>
2021-11-02 19:20:53 -0400

    add snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,57 +1,68 @@
 import Slider from "rc-slider";
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { IconButton, Snackbar } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./Palette.css";
-import "./Navbar.css"
+import "./Navbar.css";
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {format:"hex"}
-        this.handleChange = this.handleChange.bind(this)
-        
-    }
-    handleChange(e) {
-        this.setState({ format: e.target.value})
-        //if you opt to use state right away by passing in this.state, it will not have updated in time
-        //therefore using e.target.value that we just passed in for state is more immediate and prevents having to use a callback function
-        // to wrap the this.props.handlechange in a setimeout
-        this.props.handleChange(e.target.value)
-    }
-    render() {
-        const {level, changeLevel} = this.props
-        const {format} = this.state
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex", open: "true" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    //if you opt to use state right away by passing in this.state, it will not have updated in time
+    //therefore using e.target.value that we just passed in for state is more immediate and prevents having to use a callback function
+    // to wrap the this.props.handlechange in a setimeout
+    this.props.handleChange(e.target.value);
+  }
+  render() {
+    const { level, changeLevel } = this.props;
+    const { format, open } = this.state;
 
-    
-        return (
-            <header className="Navbar">
-                <div className="logo">
-                    <a href="/">reactcolorpicker</a>
-                </div>
-            <div className="slider-container">
-                <span>Level: {level}</span>
-                <div className="slider">
-                    <Slider
-                        defaultValue={level}
-                        min={100}
-                        max={900}
-                        step={100}
-                        onAfterChange={changeLevel}
-                    />
-                </div>
-            </div>
-            <div className="select-container">
-                <Select value={format} onChange={this.handleChange}>
-                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
-                    <MenuItem value= "rgb">RGB - rgb(255,255,255)</MenuItem>
-                    <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0) </MenuItem>
-                </Select>
-            </div>
-            </header>
-        )
-    }
+    return (
+      <header className="Navbar">
+        <div className="logo">
+          <a href="/">reactcolorpicker</a>
+        </div>
+        <div className="slider-container">
+          <span>Level: {level}</span>
+          <div className="slider">
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
+          </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0) </MenuItem>
+          </Select>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed!</span>}
+          ContentProps={{"aria-describedby": "message-id"}}
+          action={[
+              <IconButton>
+                <CloseIcon/>
+              </IconButton>
+          ]}
+        />
+      </header>
+    );
+  }
 }
