d65ebf76ed5f2716c43a85de183f157781e75bcc
ih <ihaddy@gmail.com>
2021-11-04 07:31:14 -0400

    introduce router functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Palette from "./Palette"
-import seedColors from './seedColors';
-import generatePalette from './chromaHelpers';
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import generatePalette from "./chromaHelpers";
+import { Route, Routes, } from "react-router-dom";
 
 function App() {
-  console.log(generatePalette(seedColors[1]))
+  console.log(generatePalette(seedColors[1]));
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])}/>
-      
-    </div> 
-    
+    //react router v6 functionality replaces render with element, using the old render{() => <h1>} test syntax is broken
+    <Routes>
+      <Route  exact path="/" element={ <Palette palette={generatePalette(seedColors[4])}/>} />
+      <Route  exact path="/" element={ <h1>test</h1>} />
+      {/* <NoMatch render={() => <Palette palette={generatePalette(seedColors[4])}/>}/> */}
+    </Routes>
   );
 }
 
-export default App;
+export default App; 
