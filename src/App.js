04596af5b85609c69c6c73079424ce377e9ca8ab
ih <ihaddy@gmail.com>
2021-11-05 08:03:49 -0400

    downgrade to router v5 to avoid functional rewrite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import generatePalette from "./chromaHelpers";
-import { Route, Routes, } from "react-router-dom";
+import { Route, Routes, useParams, useLocation, useNavigate } from "react-router-dom";
 
 function App() {
-  console.log(generatePalette(seedColors[1]));
+  // console.log(generatePalette(seedColors[1]));
+
+
+ 
+  
+  function findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+
   return (
     //react router v6 functionality replaces render with element, using the old render{() => <h1>} test syntax is broken
     <Routes>
-      <Route  exact path="/" element={ <Palette palette={generatePalette(seedColors[4])}/>} />
-      <Route  exact path="/" element={ <h1>test</h1>} />
+      <Route
+        exact
+        path="/"
+        element={<Palette palette={generatePalette(seedColors[4])} />}
+      />
+      <Route
+        exact
+        path="/palette/:id"
+        element={withRouter(<Palette params={params}
+            palette={generatePalette(findPalette(params))}
+          />
+        )}
+      />
       {/* <NoMatch render={() => <Palette palette={generatePalette(seedColors[4])}/>}/> */}
     </Routes>
   );
 }
 
-export default App; 
+export default App;
