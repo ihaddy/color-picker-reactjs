1c3e60b33b42d5856fd901a072fc7fe88590c831
ih <ihaddy@gmail.com>
2021-11-16 14:44:44 -0500

    move newpalette nav to its own component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -3,19 +3,16 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { ChromePicker } from "react-color";
 import { Button } from "@material-ui/core";
 import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 import DraggableColorList from "./DraggableColorList";
 import { arrayMove } from "react-sortable-hoc";
+import PaletteFormNav from "./PaletteFormNav";
 
 const drawerWidth = 400;
 
@@ -155,12 +152,12 @@ class NewPaletteForm extends Component {
       colors: arrayMove(colors, oldIndex, newIndex),
     }));
   };
-  savePalette() {
-    let newColorName = this.state.newPaletteName;
+  savePalette(newPaletteName) {
+    
     const newPalette = {
-      paletteName: newColorName,
+      paletteName: newPaletteName,
       colors: this.state.colors,
-      id: newColorName.toLowerCase().replace(/ /g, "-"),
+      id: newPaletteName.toLowerCase().replace(/ /g, "-"),
     };
     this.props.savePalette(newPalette);
     this.props.history.push("/");
@@ -177,48 +174,18 @@ class NewPaletteForm extends Component {
   render() {
     const { classes } = this.props;
     const { open, currentColor, colors } = this.state;
-    const paletteFull = colors.length >= this.props.maxColors
+    const paletteFull = colors.length >= this.props.maxColors;
 
     return (
       <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-          color="default"
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Persistent drawer
-            </Typography>
-            <ValidatorForm onSubmit={this.savePalette}>
-              <TextValidator
-                label="Palette Name"
-                value={this.state.newPaletteName}
-                name="newPaletteName"
-                onChange={this.handleChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={[
-                  "Please enter a Palette Name!",
-                  "Palette Name Already In Use!",
-                ]}
-              />
-              <Button variant="contained" color="primary" type="submit">
-                Save Palette
-              </Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
+        <PaletteFormNav
+          palettes={this.props.palettes}
+          open={this.state.open}
+          classes={classes}
+          newPaletteName={this.state.newPaletteName}
+          savePalette={this.savePalette}
+          handleDrawerOpen={this.handleDrawerOpen}
+        />
         <Drawer
           className={classes.drawer}
           variant="persistent"
@@ -276,7 +243,8 @@ class NewPaletteForm extends Component {
               //The button has an onClick method that bypass the validator form. Remove that method and set the button to type="submit" and it should work.
               //   onClick={this.addNewColor}
               type="submit"
-            >{paletteFull ? "The Palette is Full!" : "Add Colors"}
+            >
+              {paletteFull ? "The Palette is Full!" : "Add Colors"}
             </Button>
           </ValidatorForm>
         </Drawer>
