6ba0f689eb909b6b5d4ed4f232b1d124f6d2fd8a
ih <ihaddy@gmail.com>
2021-11-22 15:12:22 -0500

    add save palettes to browser local storage

--- src/App.js
+++ src/App.js
@@ -11,17 +11,22 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor(props) {
     super(props);
-
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"))
     this.state = {
-      palettes: seedColors,
+
+      palettes: savedPalettes || seedColors,
     };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    
   }
 
   savePalette(newPalette) {
     console.log(newPalette);
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+  }
+  syncLocalStorage(){
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes))
   }
   findPalette(id) {
     return this.state.palettes.find(function (palette) {
